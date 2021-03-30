57119e197849309c64b59bb77bf26524494f07bf
Jed Parsons <jed@jedparsons.com>
2021-03-30 11:46:23 -0700

    adding a file i forgot to add. oops.

---
+++ inc/def.h
@@ -0,0 +1,12 @@
+#ifndef __DEF_H
+#define __DEF_H
+
+#define F_NONE    0x00
+#define F_INT     0x01
+#define F_FLT     0x02
+#define F_CHR     0x04
+#define F_STR     0x40
+#define F_VAR     0x80
+
+#endif
+
