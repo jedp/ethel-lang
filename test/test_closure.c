5731c1e20f19ab10f30b22bd10fad57496f24a78
Jed Parsons <jed@jedparsons.com>
2022-07-20 22:37:00 -0700

    Test for closure generator

--- test/test_closure.c
+++ test/test_closure.c
@@ -65,9 +65,35 @@ void test_return_anonymous_closure_unbound(void) {
     TEST_ASSERT_EQUAL(15, obj->intval);
 }
 
+void test_generate_multiple_closures(void) {
+    char *program = "{                 \n"
+                    "  val m = fn() {  \n"
+                    "    var i = 0     \n"
+                    "    fn() {        \n"
+                    "      i = i + 1   \n"
+                    "      i           \n"
+                    "    }             \n"
+                    "  }               \n"
+                    "  val f = m()     \n"
+                    "  val g = m()     \n"
+                    "  f()             \n"
+                    "  f()             \n"
+                    "  f()             \n"
+                    "  f()             \n"
+                    "  g()             \n"
+                    "  g()             \n"
+                    "}";
+    eval_result_t *result = (eval_result_t *) alloc_type(EVAL_RESULT, F_NONE);
+    eval_program(program, result);
+
+    obj_t *obj = result->obj;
+    TEST_ASSERT_EQUAL(2, obj->intval);
+}
+
 void test_closure(void) {
     RUN_TEST(test_return_closure);
     RUN_TEST(test_return_closure_unbound);
     RUN_TEST(test_return_anonymous_closure);
     RUN_TEST(test_return_anonymous_closure_unbound);
+    RUN_TEST(test_generate_multiple_closures);
 }
